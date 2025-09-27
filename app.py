352cc9a09f053b623c93c8a4734e48e6c03a37ef
Thinal <r.thinal.fernando@gmail.com>
2025-09-28 01:42:10 +0530

    - connected the db with the form and makde everything work - now when a user submits the form it updates the db

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'er7gh93478htuinfs9g834'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 
 db = SQLAlchemy(app)
 
@@ -15,16 +15,31 @@ class Expense(db.Model):
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __init__(self, description, amount,data):
+    def __init__(self, description, amount,date):
         self.description = description
-        self.amount = amount
-        self.date = data
+        self.amount = float(amount)
+        self.date = datetime.strptime(date, "%Y-%m-%d")
 
 @app.route("/")
 def index():
-    return render_template ('home.html')
+    all_data = Expense.query.all()
+    return render_template ('home.html' , expenses = all_data)
 
+@app.route('/add_expense', methods = ['POST'])
+def add_expense():
+    if request.method == "POST":
+        description = request.form['description']
+        amount = request.form['amount']
+        date = request.form['date']
+
+        my_data = Expense(description, amount, date)
+        db.session.add(my_data)
+        db.session.commit()
+
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

c24252c4b4d997d4eb36b4996dc54b89f8540c69
Thinal <r.thinal.fernando@gmail.com>
2025-09-28 00:54:51 +0530

    Created a Class(model) for expenses to make a table in the database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,7 +9,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
+    def __init__(self, description, amount,data):
+        self.description = description
+        self.amount = amount
+        self.date = data
 
 @app.route("/")
 def index():

34dbc68f6451e8bda77231c95b70489abd4837b4
Thinal <r.thinal.fernando@gmail.com>
2025-09-28 00:30:20 +0530

    imported SQLALchemy and set it up with a secret key

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'er7gh93478htuinfs9g834'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
 
 
 @app.route("/")
 def index():
     return render_template ('home.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
